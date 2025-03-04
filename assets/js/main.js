e3516cfa3c5bcd0335c46dc78a484e0e84bc5b29
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2025-03-04 07:55:25 +0000

    feat(dark mode): dark mode button implemented

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,1 +1,9 @@
-/* main js */
+// Cambio a modo oscuro
+const darkModeToggle = document.getElementById("dark-mode-toggle");
+const elementsToToggle = document.querySelectorAll(".dark-mode");
+
+darkModeToggle.addEventListener("click", () => {
+    elementsToToggle.forEach(element => {
+        element.classList.toggle("dark-mode");
+    });
+});
