e939a02d953cfe653fd636cf5d03f0d2907edfed
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2025-03-06 09:00:42 +0000

    style(resposive footer): mobile footer

--- assets/js/text.js
+++ assets/js/text.js
@@ -5,7 +5,7 @@ const translations = {
     "contact-nav": "Contacto",
     "about-title": "Sobre Mí",
     "about-text-1":
-      "Hola!! mi nombre es <strong>Cristian Cabrera</strong> y soy <em>Desarrollador Web Fullstack</em> y amante de la tecnología.",
+      "Hola!! mi nombre es <strong>Cristian D. Cabrera</strong> y soy <em>Desarrollador Web Fullstack</em> y amante de la tecnología.",
     "about-text-2":
       "Soy una persona autosuficiente, responsable y comprometida que valora profundamente el trabajo en equipo y un ambiente laboral positivo.",
     "about-text-3":
@@ -44,7 +44,7 @@ const translations = {
     "project-tech-text-4": "MySQL, PHP con Symfony y Prestashop.",
     "contact-title": "Contacto",
     "contact-text":
-      "Estoy abierto a oportunidades laborales donde pueda contribuir, aprender y crecer. Si tienes una buena oportunidad que coincida con mis habilidades y experiencia, no dudes en contactarme. <a href='mailto:cristiancabrera.1985@gmail.com'>cristiancabrera.1985@gmail.com</a>",
+      "Estoy abierto a oportunidades laborales donde pueda contribuir, aprender y crecer. Si tienes una buena oportunidad que coincida con mis habilidades y experiencia, no dudes en contactarme.<br> <a href='mailto:cristiancabrera.1985@gmail.com'>cristiancabrera.1985@gmail.com</a>",
     "footer-upper-text":
       "Profesional comprometido, desarrollador apasionado, siempre con la mejor actitud.",
     "footer-lower-text":
@@ -56,7 +56,7 @@ const translations = {
     "contact-nav": "Contact",
     "about-title": "About Me",
     "about-text-1":
-      "Hello!! My name is <strong>Cristian Cabrera</strong> and I am a <em>Fullstack Web Developer</em> and a technology enthusiast.",
+      "Hello!! My name is <strong>Cristian D. Cabrera</strong> and I am a <em>Fullstack Web Developer</em> and a technology enthusiast.",
     "about-text-2":
       "I am a self-sufficient, responsible, and committed person who deeply values teamwork and a positive work environment.",
     "about-text-3":
@@ -95,7 +95,7 @@ const translations = {
     "project-tech-text-4": "MySQL, PHP with Symfony, and Prestashop.",
     "contact-title": "Contact",
     "contact-text":
-      "I am open to job opportunities where I can contribute, learn, and grow. If you have a great opportunity that matches my skills and experience, don't hesitate to contact me. <a href='mailto:cristiancabrera.1985@gmail.com'>cristiancabrera.1985@gmail.com</a>",
+      "I am open to job opportunities where I can contribute, learn, and grow. If you have a great opportunity that matches my skills and experience, don't hesitate to contact me. <br><a href='mailto:cristiancabrera.1985@gmail.com'>cristiancabrera.1985@gmail.com</a>",
     "footer-upper-text":
       "Committed professional, passionate developer, always with the best attitude.",
     "footer-lower-text":
