e3dc9060ee5701d15607ef8b0bc6a9f0ecf56d08
CristianDavidCabrera <cristiancabrera.1985@gmail.com>
2025-03-17 16:58:25 +0000

    Refactor(sidebarButton)

--- assets/js/sidebarButton.js
+++ assets/js/sidebarButton.js
@@ -2,55 +2,22 @@ const sidebar = document.getElementById("sidebar");
 const sidebarToggle = document.getElementById("sidebar-toggle");
 const sidebarIcon = document.getElementById("sidebar-icon");
 
-const sidebarHome = document.getElementById("home-sidebar");
-const sidebarAbout = document.getElementById("about-sidebar");
-const sidebarEducation = document.getElementById("education-sidebar");
-const sidebarSkill = document.getElementById("skill-sidebar");
-const sidebarProject = document.getElementById("projects-sidebar");
-const sidebarContact = document.getElementById("contact-sidebar");
-const sidebarSpanish = document.getElementById("toggle-language-tablet-es");
-const sidebarEnglish = document.getElementById("toggle-language-tablet-en");
+const sidebarItems = [
+  "home-sidebar",
+  "about-sidebar",
+  "education-sidebar",
+  "skill-sidebar",
+  "projects-sidebar",
+  "contact-sidebar",
+  "toggle-language-tablet-es",
+  "toggle-language-tablet-en",
+].map(id => document.getElementById(id));
 
-sidebarToggle.addEventListener("click", () => {
-  sidebarToggle.classList.toggle("hidden-item");
+const toggleSidebar = () => {
   sidebar.classList.toggle("open");
-});
+  sidebarToggle.classList.toggle("hidden-item");
+};
 
-sidebarIcon.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-
-sidebarHome.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-
-sidebarAbout.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarEducation.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarSkill.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarProject.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarContact.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarSpanish.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
-sidebarEnglish.addEventListener("click", () => {
-  sidebar.classList.toggle("open");
-  sidebarToggle.classList.toggle("hidden-item");
-});
+[sidebarToggle, sidebarIcon, ...sidebarItems].forEach(item => 
+  item.addEventListener("click", toggleSidebar)
+);
